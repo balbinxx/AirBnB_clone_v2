7d588ac07ea853fb689f7a675090d339814a907d
balbinxx <1815@holbertonschool.com>
2021-05-02 18:06:49 -0500

    first

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Start web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def start():
+    """Hello"""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
