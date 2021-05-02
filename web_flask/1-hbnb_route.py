eb4a099fc319b2332dbededb0373bf3c70174216
balbinxx <1815@holbertonschool.com>
2021-05-02 18:43:43 -0500

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def start():
+    """Hello"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def another():
+    """Return HBNB"""
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
