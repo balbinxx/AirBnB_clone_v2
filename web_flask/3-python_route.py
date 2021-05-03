5eaafd51101f016a04fe9d4fd4326246305dc07e
balbinxx <1815@holbertonschool.com>
2021-05-02 19:01:29 -0500

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Start web app"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def start():
+    """Hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def another():
+    """Return HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """return a message"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """return a message"""
+    return "Python {}".format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
